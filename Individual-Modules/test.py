fc888b408f127fc7e7b73abf90b2cf231207e5e8
Abhishek Anjan Pai <64316558+iicemann@users.noreply.github.com>
2020-11-30 19:23:02 +0530

    Add files via upload

---
+++ Individual-Modules/test.py
@@ -0,0 +1,15 @@
+value = 5
+
+def check(value):
+	if value == 2:
+		print('Two')
+	elif value == 3:
+		print('Three')
+
+	elif value == 4:
+		print('Four')
+
+	elif value == 5:
+		print('Five')
+
+check(value)
