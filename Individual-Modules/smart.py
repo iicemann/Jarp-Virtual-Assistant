3c8393105fb57f1b3c35e372887d3620edb7aad2
Abhishek Anjan Pai <64316558+iicemann@users.noreply.github.com>
2020-11-30 19:21:45 +0530

    Add files via upload

---
+++ Individual-Modules/smart.py
@@ -0,0 +1,14 @@
+with open('test.py') as f:
+	lines = f.readlines()
+
+with open('test.py') as f:
+    for i, l in enumerate(f):
+        pass
+print(i+1)
+
+with open('test.py', 'w') as f:
+	lines.insert(i-1, '\n	elif value == 5:')
+	lines.insert(i, '\n		print(\'Five\')\n')
+	f.write("".join(lines))
+	#f.write('\nprint(a+1)')
+	#f.write('\nif a == 1: print(\'a=1\')')
